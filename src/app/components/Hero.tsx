e234bd6ed5eba669a955d7895435e0c028770a55
Divine Obeten <divineobeten9@gmail.com>
2024-05-03 23:13:09 +0100

    add hero

---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+// const MoodButtons = [
+
+// ]
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="hero-header">
+        <h1>Lorem ipsum dolor sit amet consectetur.</h1>
+      </div>
+      <div className="hero-paragraph">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam
+          iusto voluptatibus porro dolores mollitia deserunt!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
