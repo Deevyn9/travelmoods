01ee047e9d4c895c24e5af341d7041d29f587cc6
Divine Obeten <divineobeten9@gmail.com>
2024-05-04 20:30:08 +0100

    add different moods, create anchor tags

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,20 +1,95 @@
 import React from "react";
 
-// const MoodButtons = [
-
-// ]
+const MoodButtons = [
+  {
+    moodname: "Adventrous",
+    path: "#",
+  },
+  {
+    moodname: "Relaxed",
+    path: "#",
+  },
+  {
+    moodname: "Exotic",
+    path: "#",
+  },
+  {
+    moodname: "Playful",
+    path: "#",
+  },
+  {
+    moodname: "Wild",
+    path: "#",
+  },
+  {
+    moodname: "Spiritual",
+    path: "#",
+  },
+  {
+    moodname: "Romantic",
+    path: "#",
+  },
+  {
+    moodname: "Vibrant",
+    path: "#",
+  },
+  {
+    moodname: "Scenic",
+    path: "#",
+  },
+  {
+    moodname: "Independent",
+    path: "#",
+  },
+  {
+    moodname: "Spontaneous",
+    path: "#",
+  },
+  {
+    moodname: "Epicurean",
+    path: "#",
+  },
+  {
+    moodname: "Urban",
+    path: "#",
+  },
+  {
+    moodname: "Coastal",
+    path: "#",
+  },
+  {
+    moodname: "Reverence",
+    path: "#",
+  },
+  {
+    moodname: "Natural",
+    path: "#",
+  },
+  {
+    moodname: "Vagabond",
+    path: "#",
+  },
+  {
+    moodname: "Wanderlust",
+    path: "#",
+  },
+];
 
 const Hero = () => {
   return (
     <div className="hero">
       <div className="hero-header">
-        <h1>Lorem ipsum dolor sit amet consectetur.</h1>
+        <h1>Discover great vacation destinations based on your mood</h1>
       </div>
       <div className="hero-paragraph">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam
-          iusto voluptatibus porro dolores mollitia deserunt!
-        </p>
+        <p>How are you feeling?</p>
+      </div>
+      <div className="mood-buttons">
+        {MoodButtons.map((moodButton, i) => (
+          <a href="#" key={i}>
+            {moodButton.moodname}
+          </a>
+        ))}
       </div>
     </div>
   );
