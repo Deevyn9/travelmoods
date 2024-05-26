12a8a43c9e7ba14483c5f74f61d67e72392bb775
Divine Obeten <divineobeten9@gmail.com>
2024-05-26 21:42:17 +0100

    link data

--- src/app/mood/[moods]/page.tsx
+++ src/app/mood/[moods]/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
-import React from "react";
+import { useState, useEffect } from "react";
+import moodData from "@/lib/mood-data";
 
 const MoodPage = () => {
   return (
