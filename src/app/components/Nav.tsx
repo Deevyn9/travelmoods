cd5300a76395078abeaf9f8f32c9ddccf09bf321
Divine Obeten <divineobeten9@gmail.com>
2024-06-03 21:35:56 +0100

    add links to nav

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -1,18 +1,21 @@
 import React from "react";
+import Logo from "../../../public/logo.jpg";
+import Image from "next/image";
+import Link from "next/link";
 
 const Nav = () => {
   return (
     <div className="nav">
       <div className="nav-wrapper">
-        <div className="logo">
-          <p>Logo</p>
-        </div>
+        <Link href="/" className="logo">
+          <Image src={Logo} alt="logo" width={50} height={50} />
+        </Link>
         <div className="path-name">
           <h3>TravelMood</h3>
         </div>
-        <div className="edit-mood">
-          <button>Edit Mood</button>
-        </div>
+        <Link href="/" className="edit-mood">
+          Edit Mood
+        </Link>
       </div>
     </div>
   );
