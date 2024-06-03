649fa6dec7329274f1163bb3440cef6f70b12bcc
Divine Obeten <divineobeten9@gmail.com>
2024-06-03 21:35:21 +0100

    update data

--- src/lib/mood-data.tsx
+++ src/lib/mood-data.tsx
@@ -1,115 +1,283 @@
 const moodData = [
   {
     name: "Adventurous",
-    place: {
-      name: "Queenstown, New Zealand",
-      description:
-        "A thrill-seeker's paradise with bungee jumping, skydiving, and white-water rafting.",
-    },
+    places: [
+      {
+        name: "Queenstown, New Zealand",
+        description:
+          "A thrill-seeker's paradise with bungee jumping, skydiving, and white-water rafting.",
+      },
+      {
+        name: "Queenstown, New Zealand",
+        description:
+          "A thrill-seeker's paradise with bungee jumping, skydiving, and white-water rafting.",
+      },
+      {
+        name: "Queenstown, New Zealand",
+        description:
+          "A thrill-seeker's paradise with bungee jumping, skydiving, and white-water rafting.",
+      },
+    ],
   },
   {
     name: "Relaxed",
-    place: {
-      name: "Bali, Indonesia",
-      description:
-        "A tropical haven with serene beaches, yoga retreats, and luxurious spas.",
-    },
+    places: [
+      {
+        name: "Bali, Indonesia",
+        description:
+          "A tropical haven with serene beaches, yoga retreats, and luxurious spas.",
+      },
+      {
+        name: "Bali, Indonesia",
+        description:
+          "A tropical haven with serene beaches, yoga retreats, and luxurious spas.",
+      },
+      {
+        name: "Bali, Indonesia",
+        description:
+          "A tropical haven with serene beaches, yoga retreats, and luxurious spas.",
+      },
+    ],
   },
   {
     name: "Exotic",
-    place: {
-      name: "Marrakech, Morocco",
-      description:
-        "A vibrant souk with colorful markets, aromatic spices, and stunning architecture.",
-    },
+    places: [
+      {
+        name: "Marrakech, Morocco",
+        description:
+          "A vibrant souk with colorful markets, aromatic spices, and stunning architecture.",
+      },
+      {
+        name: "Marrakech, Morocco",
+        description:
+          "A vibrant souk with colorful markets, aromatic spices, and stunning architecture.",
+      },
+      {
+        name: "Marrakech, Morocco",
+        description:
+          "A vibrant souk with colorful markets, aromatic spices, and stunning architecture.",
+      },
+    ],
   },
   {
     name: "Playful",
-    place: {
-      name: "Orlando, Florida",
-      description:
-        "A theme park paradise with Disney World, Universal Studios, and Legoland.",
-    },
+    places: [
+      {
+        name: "Orlando, Florida",
+        description:
+          "A theme park paradise with Disney World, Universal Studios, and Legoland.",
+      },
+      {
+        name: "Orlando, Florida",
+        description:
+          "A theme park paradise with Disney World, Universal Studios, and Legoland.",
+      },
+      {
+        name: "Orlando, Florida",
+        description:
+          "A theme park paradise with Disney World, Universal Studios, and Legoland.",
+      },
+    ],
   },
   {
     name: "Wild",
-    place: {
-      name: "Yellowstone National Park, USA",
-      description:
-        "A wilderness wonderland with geysers, hot springs, and diverse wildlife.",
-    },
+    places: [
+      {
+        name: "Yellowstone National Park, USA",
+        description:
+          "A wilderness wonderland with geysers, hot springs, and diverse wildlife.",
+      },
+      {
+        name: "Yellowstone National Park, USA",
+        description:
+          "A wilderness wonderland with geysers, hot springs, and diverse wildlife.",
+      },
+      {
+        name: "Yellowstone National Park, USA",
+        description:
+          "A wilderness wonderland with geysers, hot springs, and diverse wildlife.",
+      },
+    ],
   },
   {
     name: "Spiritual",
-    place: {
-      name: "Varanasi, India",
-      description:
-        "A sacred city with ancient temples, river Ganges, and spiritual enlightenment.",
-    },
+    places: [
+      {
+        name: "Varanasi, India",
+        description:
+          "A sacred city with ancient temples, river Ganges, and spiritual enlightenment.",
+      },
+      {
+        name: "Varanasi, India",
+        description:
+          "A sacred city with ancient temples, river Ganges, and spiritual enlightenment.",
+      },
+      {
+        name: "Varanasi, India",
+        description:
+          "A sacred city with ancient temples, river Ganges, and spiritual enlightenment.",
+      },
+    ],
   },
   {
     name: "Romantic",
-    place: {
-      name: "Paris, France",
-      description:
-        "The City of Love with iconic Eiffel Tower, charming streets, and luxurious getaways.",
-    },
+    places: [
+      {
+        name: "Paris, France",
+        description:
+          "The City of Love with iconic Eiffel Tower, charming streets, and luxurious getaways.",
+      },
+      {
+        name: "Paris, France",
+        description:
+          "The City of Love with iconic Eiffel Tower, charming streets, and luxurious getaways.",
+      },
+      {
+        name: "Paris, France",
+        description:
+          "The City of Love with iconic Eiffel Tower, charming streets, and luxurious getaways.",
+      },
+    ],
   },
   {
     name: "Vibrant",
-    place: {
-      name: "Rio de Janeiro, Brazil",
-      description:
-        "A Carnival city with colorful streets, stunning beaches, and lively nightlife.",
-    },
+    places: [
+      {
+        name: "Rio de Janeiro, Brazil",
+        description:
+          "A Carnival city with colorful streets, stunning beaches, and lively nightlife.",
+      },
+      {
+        name: "Rio de Janeiro, Brazil",
+        description:
+          "A Carnival city with colorful streets, stunning beaches, and lively nightlife.",
+      },
+      {
+        name: "Rio de Janeiro, Brazil",
+        description:
+          "A Carnival city with colorful streets, stunning beaches, and lively nightlife.",
+      },
+    ],
   },
   {
     name: "Scenic",
-    place: {
-      name: "Ha Long Bay, Vietnam",
-      description:
-        "A breathtaking archipelago with emerald waters, limestone karsts, and scenic boat tours.",
-    },
+    places: [
+      {
+        name: "Ha Long Bay, Vietnam",
+        description:
+          "A breathtaking archipelago with emerald waters, limestone karsts, and scenic boat tours.",
+      },
+      {
+        name: "Orlando, Florida",
+        description:
+          "A theme park paradise with Disney World, Universal Studios, and Legoland.",
+      },
+      {
+        name: "Orlando, Florida",
+        description:
+          "A theme park paradise with Disney World, Universal Studios, and Legoland.",
+      },
+    ],
   },
   {
     name: "Independent",
-    place: {
-      name: "Reykjavik, Iceland",
-      description:
-        "A hip city with vibrant culture, stunning landscapes, and solo traveler vibes.",
-    },
+    places: [
+      {
+        name: "Reykjavik, Iceland",
+        description:
+          "A hip city with vibrant culture, stunning landscapes, and solo traveler vibes.",
+      },
+      {
+        name: "Reykjavik, Iceland",
+        description:
+          "A hip city with vibrant culture, stunning landscapes, and solo traveler vibes.",
+      },
+      {
+        name: "Reykjavik, Iceland",
+        description:
+          "A hip city with vibrant culture, stunning landscapes, and solo traveler vibes.",
+      },
+    ],
   },
   {
     name: "Spontaneous",
-    place: {
-      name: "New Orleans, Louisiana",
-      description:
-        "A jazz city with lively streets, impromptu performances, and carefree atmosphere.",
-    },
+    places: [
+      {
+        name: "New Orleans, Louisiana",
+        description:
+          "A jazz city with lively streets, impromptu performances, and carefree atmosphere.",
+      },
+      {
+        name: "New Orleans, Louisiana",
+        description:
+          "A jazz city with lively streets, impromptu performances, and carefree atmosphere.",
+      },
+      {
+        name: "New Orleans, Louisiana",
+        description:
+          "A jazz city with lively streets, impromptu performances, and carefree atmosphere.",
+      },
+    ],
   },
   {
     name: "Epicurean",
-    place: {
-      name: "Tuscany, Italy",
-      description:
-        "A foodie's paradise with vineyards, olive groves, and gourmet cuisine.",
-    },
+    places: [
+      {
+        name: "Tuscany, Italy",
+        description:
+          "A foodie's paradise with vineyards, olive groves, and gourmet cuisine.",
+      },
+      {
+        name: "Tuscany, Italy",
+        description:
+          "A foodie's paradise with vineyards, olive groves, and gourmet cuisine.",
+      },
+      {
+        name: "Tuscany, Italy",
+        description:
+          "A foodie's paradise with vineyards, olive groves, and gourmet cuisine.",
+      },
+    ],
   },
   {
     name: "Urban",
-    place: {
-      name: "Tokyo, Japan",
-      description:
-        "A futuristic city with neon lights, bustling streets, and cutting-edge technology.",
-    },
+    places: [
+      {
+        name: "Tokyo, Japan",
+        description:
+          "A futuristic city with neon lights, bustling streets, and cutting-edge technology.",
+      },
+      {
+        name: "Tokyo, Japan",
+        description:
+          "A futuristic city with neon lights, bustling streets, and cutting-edge technology.",
+      },
+      {
+        name: "Tokyo, Japan",
+        description:
+          "A futuristic city with neon lights, bustling streets, and cutting-edge technology.",
+      },
+    ],
   },
   {
     name: "Coastal",
-    place: {
-      name: "Santorini, Greece",
-      description:
-        "A picturesque island with whitewashed houses, blue domes, and stunning sunsets.",
-    },
+    places: [
+      {
+        name: "Santorini, Greece",
+        description:
+          "A picturesque island with whitewashed houses, blue domes, and stunning sunsets.",
+      },
+      {
+        name: "Santorini, Greece",
+        description:
+          "A picturesque island with whitewashed houses, blue domes, and stunning sunsets.",
+      },
+      {
+        name: "Santorini, Greece",
+        description:
+          "A picturesque island with whitewashed houses, blue domes, and stunning sunsets.",
+      },
+    ],
   },
   {
     name: "Reverence",
@@ -118,30 +286,73 @@ const moodData = [
       description:
         "A sacred city with ancient history, religious landmarks, and spiritual significance.",
     },
+    places: [
+      {
+        name: "Jerusalem, Israel",
+        description:
+          "A sacred city with ancient history, religious landmarks, and spiritual significance.",
+      },
+      {
+        name: "Jerusalem, Israel",
+        description:
+          "A sacred city with ancient history, religious landmarks, and spiritual significance.",
+      },
+      {
+        name: "Jerusalem, Israel",
+        description:
+          "A sacred city with ancient history, religious landmarks, and spiritual significance.",
+      },
+    ],
   },
   {
     name: "Guilty",
-    place: {
-      name: "Las Vegas, Nevada",
-      description:
-        "A sinful city with casinos, shows, and indulgent pleasures.",
-    },
+    places: [
+      {
+        name: "Orlando, Florida",
+        description:
+          "A theme park paradise with Disney World, Universal Studios, and Legoland.",
+      },
+    ],
   },
   {
     name: "Vagabond",
-    place: {
-      name: "Chiang Mai, Thailand",
-      description:
-        "A digital nomad hub with affordable living, rich culture, and adventurous spirit.",
-    },
+    places: [
+      {
+        name: "Chiang Mai, Thailand",
+        description:
+          "A digital nomad hub with affordable living, rich culture, and adventurous spirit.",
+      },
+      {
+        name: "Chiang Mai, Thailand",
+        description:
+          "A digital nomad hub with affordable living, rich culture, and adventurous spirit.",
+      },
+      {
+        name: "Chiang Mai, Thailand",
+        description:
+          "A digital nomad hub with affordable living, rich culture, and adventurous spirit.",
+      },
+    ],
   },
   {
     name: "Wanderlust",
-    place: {
-      name: "The Great Ocean Road, Australia",
-      description:
-        "A scenic drive with stunning coastlines, iconic landmarks, and endless exploration.",
-    },
+    places: [
+      {
+        name: "The Great Ocean Road, Australia",
+        description:
+          "A scenic drive with stunning coastlines, iconic landmarks, and endless exploration.",
+      },
+      {
+        name: "The Great Ocean Road, Australia",
+        description:
+          "A scenic drive with stunning coastlines, iconic landmarks, and endless exploration.",
+      },
+      {
+        name: "The Great Ocean Road, Australia",
+        description:
+          "A scenic drive with stunning coastlines, iconic landmarks, and endless exploration.",
+      },
+    ],
   },
 ];
 
