2eedd5d767380f59e5a2e5cb4daae2e81412a286
Divine Obeten <divineobeten9@gmail.com>
2024-06-03 21:35:42 +0100

    allow multiple destinations

--- src/app/mood/[moods]/page.tsx
+++ src/app/mood/[moods]/page.tsx
@@ -10,25 +10,48 @@ const MoodPage = () => {
   const params = useParams();
   console.log(params);
   const moodName = params.moods;
-  console.log(moodName);
+  //   console.log(moodName);
   //   console.log(moodData.find((mood) => mood.name === params.moods));
   const currentMoodData = moodData.find((mood) => mood.name === moodName);
-  console.log(currentMoodData);
+  //   console.log(currentMoodData);
+  // const getPlaces = (moodName: string) => {
+  //     return moodData.find((mood) => mood.name === moodName)?.place;
+  // };
+  const getPlaces = (moodName: string | string[]) => {
+    if (Array.isArray(moodName)) {
+      // If moodName is an array, return places for all moods in the array
+      return moodName.flatMap(
+        (mood) =>
+          moodData.find((moodData) => moodData.name === mood)?.places ?? []
+      );
+    } else {
+      // If moodName is a single string, return places for that mood
+      return (
+        moodData.find((moodData) => moodData.name === moodName)?.places ?? []
+      );
+    }
+  };
+
+  const places = getPlaces(moodName);
 
   return (
     <div className="">
       <div className="container">
-        <div className="image-area">
-          <Image src={igm} alt="video" />
-        </div>
-        <div className="details-area">
-          <div className="destination-name">
-            <h2>{currentMoodData?.place.name}</h2>
-          </div>
-          <div className="description">
-            <p>ths: {currentMoodData?.place.description}</p>
-          </div>
-        </div>
+        {places.map((place, index) => (
+          <>
+            <div className="image-area" key={index}>
+              <Image src={igm} alt="video" />
+            </div>
+            <div className="details-area">
+              <div className="destination-name">
+                <h2>{place.name}</h2>
+              </div>
+              <div className="description">
+                <p>ths: {place.description}</p>
+              </div>
+            </div>
+          </>
+        ))}
         <div className="controls">
           <div className="back-button">
             <button>back</button>
