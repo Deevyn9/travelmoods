9135e8ec07c9de5dcf2df0dfc8437b2a426ee746
Divine Obeten <divineobeten9@gmail.com>
2024-06-04 16:46:30 +0100

    add destination randomization and enable direction buttons

--- src/app/mood/[moods]/page.tsx
+++ src/app/mood/[moods]/page.tsx
@@ -1,66 +1,72 @@
 "use client";
 import Image from "next/image";
-// import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "next/navigation";
 import moodData from "@/lib/mood-data";
 import igm from "../../../../public/vercel.svg";
 
 const MoodPage = () => {
-  console.log(moodData);
   const params = useParams();
-  console.log(params);
   const moodName = params.moods;
-  //   console.log(moodName);
-  //   console.log(moodData.find((mood) => mood.name === params.moods));
-  const currentMoodData = moodData.find((mood) => mood.name === moodName);
-  //   console.log(currentMoodData);
-  // const getPlaces = (moodName: string) => {
-  //     return moodData.find((mood) => mood.name === moodName)?.place;
-  // };
-  const getPlaces = (moodName: string | string[]) => {
-    if (Array.isArray(moodName)) {
-      // If moodName is an array, return places for all moods in the array
-      return moodName.flatMap(
-        (mood) =>
-          moodData.find((moodData) => moodData.name === mood)?.places ?? []
-      );
-    } else {
-      // If moodName is a single string, return places for that mood
-      return (
-        moodData.find((moodData) => moodData.name === moodName)?.places ?? []
-      );
-    }
+  const [places, setPlaces] = useState<{ name: string; description: string }[]>(
+    []
+  );
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const getPlaces = (moodName: string | string[]) => {
+      if (Array.isArray(moodName)) {
+        return moodName.flatMap(
+          (mood) =>
+            moodData.find((moodData) => moodData.name === mood)?.places ?? []
+        );
+      } else {
+        return (
+          moodData.find((moodData) => moodData.name === moodName)?.places ?? []
+        );
+      }
+    };
+    const shuffledPlaces = getPlaces(moodName).sort(() => Math.random() - 0.5);
+    setPlaces(shuffledPlaces);
+  }, [moodName, currentIndex]);
+
+  const handleNext = () => {
+    setCurrentIndex(currentIndex + 1);
   };
 
-  const places = getPlaces(moodName);
+  const handleBack = () => {
+    setCurrentIndex(currentIndex - 1);
+  };
 
   return (
     <div className="">
       <div className="container">
-        {places.map((place, index) => (
-          <>
-            <div className="image-area" key={index}>
-              <Image src={igm} alt="video" />
-            </div>
-            <div className="details-area">
-              <div className="destination-name">
-                <h2>{place.name}</h2>
+        <div className="destinations">
+          {places.map((place, index) => (
+            <div key={index}>
+              <div className="image-area">
+                {/* <Image src={igm} alt="video" /> */}
               </div>
-              <div className="description">
-                <p>ths: {place.description}</p>
+              <div className="details-area">
+                <div className="destination-name">
+                  <h2>{place.name}</h2>
+                </div>
+                <div className="description">
+                  <p>{place.description}</p>
+                </div>
               </div>
             </div>
-          </>
-        ))}
+          ))}
+        </div>
         <div className="controls">
           <div className="back-button">
-            <button>back</button>
+            <button onClick={() => handleBack()}>back</button>
           </div>
           <div className="hide-button">
             <button>hide</button>
           </div>
           <div className="next-button">
-            <button>next</button>
+            <button onClick={() => handleNext()}>next</button>
           </div>
         </div>
       </div>
