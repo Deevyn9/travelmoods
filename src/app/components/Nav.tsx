aaa5647a6fad22ddb6705dfb6d5840a10463911d
Divine Obeten <divineobeten9@gmail.com>
2024-06-09 23:06:35 +0100

    conditionally render edit mood button

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -1,19 +1,30 @@
+"use client";
 import React from "react";
 import Logo from "../../../public/beach icon.png";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Nav = () => {
+  const route = usePathname();
+  const isHomePage = route === "/";
+
   return (
     <div className="nav">
       <div className="nav-wrapper">
         <Link href="/" className="logo">
-          <Image src={Logo} alt="logo" width={60} height={60} />
+          <Image src={Logo} alt="logo" />
         </Link>
         <div className="path-name">
           <h3>TravelMoods</h3>
         </div>
-        <Link href="/" className="edit-mood">
+        <Link
+          href="/"
+          className="edit-mood"
+          style={{
+            opacity: isHomePage ? 0 : 1,
+          }}
+        >
           Edit Mood
         </Link>
       </div>
